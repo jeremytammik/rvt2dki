4869c7983e0655b55c4f1e5faf071a3bb86c9570
Jeremy Tammik <jeremy.tammik@autodesk.com>
2023-12-22 19:58:59 +0100

    implemented DeleteAllCuttingElements and gross area determination

--- rvt2dki/Command.cs
+++ rvt2dki/Command.cs
@@ -12,10 +12,10 @@ using System.Diagnostics;
 
 namespace rvt2dki
 {
-  [Transaction(TransactionMode.ReadOnly)]
+  [Transaction(TransactionMode.Manual)]
   public class Command : IExternalCommand
   {
-    const BuiltInParameter bipArea 
+    const BuiltInParameter bipArea
       = BuiltInParameter.HOST_AREA_COMPUTED;
     const double inch = 0.0254; // metres
     const double foot = 12 * inch;
@@ -43,9 +43,23 @@ namespace rvt2dki
       foreach (Element e in walls)
       {
         double a = e.get_Parameter(bipArea).AsDouble();
-        Debug.Print($"{e.Name}: {(a * foot2):#0.00}");
+        Debug.Print($"{e.Name}: net {(a * foot2):#0.00}");
         ElementArea elarea = new ElementArea(e.Id.Value, a);
         wallAreas.Add(e.Id, elarea);
+      }
+
+      using (Transaction tx = new Transaction(doc))
+      {
+        tx.Start("Delete openings for gross wall area determination");
+        DeleteAllCuttingElements(doc);
+        doc.Regenerate();
+        foreach (Element e in walls)
+        {
+          double a = e.get_Parameter(bipArea).AsDouble();
+          Debug.Print($"{e.Name}: gross {(a * foot2):#0.00}");
+          wallAreas[e.Id].AreaGross = a;
+        }
+        tx.RollBack();
       }
 
       Dictionary<ElementId, ElementArea> floorAreas
@@ -77,12 +91,59 @@ namespace rvt2dki
       foreach (Element e in roofs)
       {
         double a = e.get_Parameter(bipArea).AsDouble();
-        Debug.Print($"{e.Name}: {(a*foot2):#0.00}");
+        Debug.Print($"{e.Name}: {(a * foot2):#0.00}");
         ElementArea elarea = new ElementArea(e.Id.Value, a);
         roofAreas.Add(e.Id, elarea);
 
       }
       return Result.Succeeded;
     }
+
+    /// <summary>
+    /// Delete all elements that cut out of target elements, 
+    /// to allow for calculation of gross material quantities.
+    /// </summary>
+    private void DeleteAllCuttingElements(Document doc)
+    {
+      // (Type == FamilyInstance && (Category == Door || Category == Window) || Type == Opening
+      ElementClassFilter filterFamilyInstance = new ElementClassFilter(typeof(FamilyInstance));
+      ElementCategoryFilter filterWindowCategory = new ElementCategoryFilter(BuiltInCategory.OST_Windows);
+      ElementCategoryFilter filterDoorCategory = new ElementCategoryFilter(BuiltInCategory.OST_Doors);
+      LogicalOrFilter filterDoorOrWindowCategory = new LogicalOrFilter(filterWindowCategory, filterDoorCategory);
+      LogicalAndFilter filterDoorWindowInstance = new LogicalAndFilter(filterDoorOrWindowCategory, filterFamilyInstance);
+
+      ElementClassFilter filterOpening = new ElementClassFilter(typeof(Opening));
+
+      LogicalOrFilter filterCuttingElements = new LogicalOrFilter(filterOpening, filterDoorWindowInstance);
+      FilteredElementCollector collector = new FilteredElementCollector(doc).WherePasses(filterCuttingElements);
+
+      foreach (Element e in collector)
+      {
+        // Doors in curtain grid systems cannot be deleted.  This doesn't actually affect the calculations because
+        // material quantities are not extracted for curtain systems.
+        if (e.Category != null)
+        {
+          if (e.Category.BuiltInCategory == BuiltInCategory.OST_Doors)
+          {
+            FamilyInstance door = e as FamilyInstance;
+            Wall host = door.Host as Wall;
+
+            if (host.CurtainGrid != null)
+              continue;
+          }
+          ICollection<ElementId> deletedElements = doc.Delete(e.Id);
+
+          // Log failed deletion attempts to the output.
+          // There may be other situations where deletion is not possible
+          // but the failure doesn't really affect the results.
+          if (deletedElements == null || deletedElements.Count < 1)
+          {
+            Debug.Print("The tool was unable to delete the {0} named {2} (id {1})", 
+              e.GetType().Name, e.Id, e.Name);
+          }
+        }
+      }
+    }
+
   }
 }
