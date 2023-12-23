9b55f252cecbe08ebb5d02bd3282cfb8b01f0121
Jeremy Tammik <jeremy.tammik@autodesk.com>
2023-12-23 13:59:16 +0100

    single element area collection and populate commenmts

--- rvt2dki/Command.cs
+++ rvt2dki/Command.cs
@@ -2,10 +2,7 @@
 using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
-using Autodesk.Revit.DB.Architecture;
 using Autodesk.Revit.UI;
-using Autodesk.Revit.UI.Selection;
-using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 #endregion
